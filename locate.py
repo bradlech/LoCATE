989765400a5fc4fad9afc63cff10d6d72e8b40d3
bradlech <cole.hamilton.bradley@gmail.com>
2018-02-10 20:07:56 +0000

    Changing variables in locate.py

--- locate.py
+++ locate.py
@@ -2,14 +2,21 @@ import os
 import subprocess
 
 # Terminal commands in use
-tshark = "sudo tshark -i wlan0 -Y wlan.fc.type_subtype==0x08 -T fields -e frame.time -e wlan.bssid -e radiotap.dbm_antsignal -e wlan_mgt.ssid -a duration:6"
+wlan0_down = "sudo ifconfig wlan0 down"
+wlan0_mode = "sudo iwconfig wlan0 mode monitor"
+wlan0_up = "sudo ifconfig wlan0 up"
+tshark_start = "sudo tshark -i wlan0 -Y wlan.fc.type_subtype==0x08 -T fields -e frame.time -e wlan.bssid -e radiotap.dbm_antsignal -e wlan_mgt.ssid -a duration:6"
+tshark_fields = ""
+tshark_end = ""
 
 # Setup wlan interfaces
-#os.system(sudo ifconfig )
-Print "Wlan interfaces ready."
+#os.system(wlan0_down)
+#os.system(wlan0_mode)
+#os.system(wlan0_up)
+print "Wlan interfaces ready."
 
 # Redirect tshark output using subprocess
-proc = subprocess.Popen([tshark], stdout=subprocess.PIPE, shell=True)
+proc = subprocess.Popen([tshark_start + tshark_fields + tshark_end], stdout=subprocess.PIPE, shell=True)
 (out, err) = proc.communicate()
 print "output:\n", out
 
